0da41884834c99ff2c53c0fcb06165858292c3a8
MobinAnsari <mobinansari.etc@gmail.com>
2017-05-11 16:19:09 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,3 +6,6 @@ for i in $@;          #Will take hostnames seperated by comma as a input
 do
 `ssh root@$i $usercommand`  # will make ssh connection with host server and execute the command on all the servers on by one.
 done
+
+# We can execute the script using below command
+#./script.sh server1,server2,server3
